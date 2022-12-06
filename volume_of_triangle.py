59a46e0373c0533cbad7d79cfeb2d57e302e3e2a
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-12-06 16:03:50 +0000

    added file

--- volume_of_triangle.py
+++ volume_of_triangle.py
@@ -21,14 +21,16 @@ def main():
     base_from_user = input("Enter the base of the triangle (cm): ")
     height_from_user = input("Enter the height of the triangle (cm): ")
     length_from_user = input("Enter length of the triangle (cm): ")
-    
+
     try:
         base_from_user = int(base_from_user)
         height_from_user = int(height_from_user)
         length_from_user = int(length_from_user)
 
         # Call function
-        final_volume = triangle_volume_calculation(base_from_user, height_from_user, length_from_user)
+        final_volume = triangle_volume_calculation(
+            base_from_user, height_from_user, length_from_user
+        )
         print(
             "\nThe volume of a triangle with the base of {0} cm, the height of {1}, and length of {2} cm is {3} cm³.".format(
                 base_from_user, height_from_user, length_from_user, final_volume
